1b00963f1d74ae279587151172aa3404c80329b8
Stuart Taylor <staylor1012@gmail.com>
2018-06-29 12:11:19 +0100

    Update get-emails to include the unsubscribeId

--- get-emails.js
+++ get-emails.js
@@ -1,33 +1,82 @@
 /* eslint-disable no-process-exit */
-require('dotenv').load();
+require('dotenv').config();
+
+const Stream = require('stream');
 const fs = require('fs');
 const assert = require('assert');
 const mongodb = require('mongodb');
-const map = require('map-stream');
 const validator = require('validator');
-const through = require('through2');
+const emailValidator = require('email-validator');
 
 const MongoClient = mongodb.MongoClient;
+const filePath = process.argv[2];
 
-const filepath = process.argv[2];
+const validOutput = fs.createWriteStream(filePath, { encoding: 'utf8' });
+const invalidOutput = fs.createWriteStream('./invalidEmails.csv', { encoding: 'utf8' });
+
+validOutput.write('email,unsubscribeId\n');
+invalidOutput.write('email,unsubscribeId\n');
+
+const rs = new Stream.Readable({objectMode: true})
+rs._read = function() {}
+
+rs.on('data', ({email, unsubscribeId}) => {
+  if(validator.isEmail(email)) {
+    validOutput.write(`${email},${unsubscribeId}\n`);
+  } else if (emailValidator.validate(email)) {
+    validOutput.write(`${email},${unsubscribeId}\n`);
+  } else {
+    invalidOutput.write(`${email},${unsubscribeId}\n`);
+  }
+})
 
 assert(
-  filepath,
+  filePath,
   `
   This script must be called with a filepath argument like so:
-  node ./get-emails ./emails.csv
+
+  yarn mailing-list ./emails.csv
+
   `
 );
 
-const writeStream = fs.createWriteStream(filepath);
-writeStream.write('email\n');
-
-MongoClient.connect(process.env.MONGODB_URI, function(err, database) {
+MongoClient.connect(process.env.MONGODB_URI, { useNewUrlParser: true }, function(err, client) {
+  const db = client.db(process.env.MONGO_DB)
   if (err) {
     throw err;
   }
 
-  database.collection('user').aggregate([
+  const stream = db.collection('user').find({
+    $and: [
+      { email: { $exists: true } },
+      { email: { $ne: '' } },
+      { email: { $ne: null } },
+      { email: { $not: /(test|fake)/i } },
+      { $or: [
+          { sendQuincyEmail: true },
+          { sendQuincyEmail: { $exists: false } },
+          { sendQuincyEmail: null }
+        ]
+      }
+    ]
+  }, {
+    email: 1,
+    unsubscribeId: 1
+  }).batchSize(100).stream();
+
+  stream.on('data', ({email, unsubscribeId}) => {
+    const data = {email, unsubscribeId};
+    rs.push(data)
+  })
+
+  stream.on('end', () => {rs.push(null);client.close()});
+
+
+});
+
+
+/**
+ * [
     { $match: { email: { $exists: true } } },
     { $match: { email: { $ne: '' } } },
     { $match: { email: { $ne: null } } },
@@ -41,24 +90,6 @@ MongoClient.connect(process.env.MONGODB_URI, function(err, database) {
         ]
       }
     },
-    { $project: { _id: 0, email: 1 } }
-  ])
-    .batchSize(10)
-    .pipe(map((user, cb) => cb(null, user.email)))
-    .pipe(through.obj((email, _, cb) => {
-      if (validator.isEmail(email)) {
-        return cb(null, email);
-      }
-      return cb();
-    }))
-    .pipe(map((email, cb) => cb(null, '' + email + '\n')))
-    .pipe(writeStream)
-    .on('finish', () => {
-      console.log(`
-        Process complete. Emails have been written to ${filepath}.
-      `);
-      process.exit(0);
-    });
-});
-
-
+    { $project: { _id: 0, email: 1, unsubscribeId: 1 } }
+  ]
+ */
