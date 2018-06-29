ef83cf11d44a500209e5c991dc96e54c2a2c4d52
Stuart Taylor <staylor1012@gmail.com>
2018-06-29 10:11:18 +0100

    Mark out of date scripts for re-writing

--- get-certified.js
+++ get-certified.js
@@ -1,34 +1,36 @@
+console.log('\n\nThis script needs to be re-written\n\n');
+
 /* eslint-disable no-process-exit */
-require('dotenv').load();
-var mongodb = require('mongodb');
-var MongoClient = mongodb.MongoClient;
+// require('dotenv').load();
+// var mongodb = require('mongodb');
+// var MongoClient = mongodb.MongoClient;
 
-MongoClient.connect(process.env.MONGODB_URI, function(err, database) {
-  if (err) {
-    throw err;
-  }
+// MongoClient.connect(process.env.MONGODB_URI, function(err, database) {
+//   if (err) {
+//     throw err;
+//   }
 
-  database.collection('user').aggregate([
-    {
-      $match: {
-        $and: [
-          { isFrontEndCert: true },
-          { isBackEndCert: true },
-          { isDataVisCert: true }
-        ]
-      }
-    },
-    {
-      $group: {
-        _id: 1,
-        usernames: { $addToSet: '$username' }
-      }
-    }
-  ], function(err, results) {
-    if (err) { throw err; }
+//   database.collection('user').aggregate([
+//     {
+//       $match: {
+//         $and: [
+//           { isFrontEndCert: true },
+//           { isBackEndCert: true },
+//           { isDataVisCert: true }
+//         ]
+//       }
+//     },
+//     {
+//       $group: {
+//         _id: 1,
+//         usernames: { $addToSet: '$username' }
+//       }
+//     }
+//   ], function(err, results) {
+//     if (err) { throw err; }
 
-    console.log('\n@' + results[0].usernames.join('\n@'));
-    // console.log(results[0].usernames.length);
-    process.exit(0);
-  });
-});
+//     console.log('\n@' + results[0].usernames.join('\n@'));
+//     // console.log(results[0].usernames.length);
+//     process.exit(0);
+//   });
+// });
--- get-invalid-emails.js
+++ get-invalid-emails.js
@@ -1,54 +1,56 @@
-/* eslint-disable no-process-exit */
-require('dotenv').load();
-const fs = require('fs');
-const assert = require('assert');
-const mongodb = require('mongodb');
-const map = require('map-stream');
-const validator = require('validator');
-const through = require('through2');
+console.log('\n\nThis script needs to be re-written\n\n');
 
-const MongoClient = mongodb.MongoClient;
+// /* eslint-disable no-process-exit */
+// require('dotenv').load();
+// const fs = require('fs');
+// const assert = require('assert');
+// const mongodb = require('mongodb');
+// const map = require('map-stream');
+// const validator = require('validator');
+// const through = require('through2');
 
-const filepath = process.argv[2];
+// const MongoClient = mongodb.MongoClient;
 
-assert(
-  filepath,
-  `
-  This script must be called with a filepath argument like so:
-  node ./get-emails ./emails.csv
-  `
-);
+// const filepath = process.argv[2];
 
-const writeStream = fs.createWriteStream(filepath);
-writeStream.write('id, email\n');
+// assert(
+//   filepath,
+//   `
+//   This script must be called with a filepath argument like so:
+//   node ./get-emails ./emails.csv
+//   `
+// );
 
-MongoClient.connect(process.env.MONGODB_URI, function(err, database) {
-  if (err) {
-    throw err;
-  }
+// const writeStream = fs.createWriteStream(filepath);
+// writeStream.write('id, email\n');
 
-  const users = [];
-  database.collection('user').aggregate([
-    { $match: { email: { $exists: true } } },
-    { $match: { email: { $ne: '' } } },
-    { $match: { email: { $ne: null } } },
-    { $project: { _id: 1, email: 1 } }
-  ])
-    .batchSize(10)
-    .pipe(through.obj((user, _, cb) => {
-      if (validator.isEmail(user.email)) {
-        return cb();
-      }
-      users.push(user);
-      return cb();
-    }))
-    .on('finish', () => {
-      fs.writeFileSync(filepath, JSON.stringify(users, null, 2));
-      console.log(`
-        Process complete. Emails have been written to ${filepath}.
-      `);
-      process.exit(0);
-    });
-});
+// MongoClient.connect(process.env.MONGODB_URI, function(err, database) {
+//   if (err) {
+//     throw err;
+//   }
+
+//   const users = [];
+//   database.collection('user').aggregate([
+//     { $match: { email: { $exists: true } } },
+//     { $match: { email: { $ne: '' } } },
+//     { $match: { email: { $ne: null } } },
+//     { $project: { _id: 1, email: 1 } }
+//   ])
+//     .batchSize(10)
+//     .pipe(through.obj((user, _, cb) => {
+//       if (validator.isEmail(user.email)) {
+//         return cb();
+//       }
+//       users.push(user);
+//       return cb();
+//     }))
+//     .on('finish', () => {
+//       fs.writeFileSync(filepath, JSON.stringify(users, null, 2));
+//       console.log(`
+//         Process complete. Emails have been written to ${filepath}.
+//       `);
+//       process.exit(0);
+//     });
+// });
 
 
