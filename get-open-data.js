b5ea21fc61a50d0b455219074ac685593d5e7f70
Stuart Taylor <staylor1012@gmail.com>
2018-06-29 10:09:41 +0100

    Tidy up and move to yarn

--- get-open-data.js
+++
@@ -1,8 +0,0 @@
-require('dotenv').load();
-var secrets = require('../config/secrets'),
-    mongodb = require('mongodb'),
-    MongoClient = mongodb.MongoClient;
-
-MongoClient.connect(secrets.db, function(err, db) {
-
-});
