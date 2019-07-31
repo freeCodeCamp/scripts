e9b2c47dbfe4da0e7da8a1f0bc6420db4c3da6d6
Randell Dawson <rdawson@onepathtech.com>
2019-07-31 09:17:59 -0700

    fix: script to validate articles have main header

---
+++ packages/forum-api-scripts/validate-guide-articles-have-main-headers.js
@@ -0,0 +1,23 @@
+const fs = require('fs');
+const { getGuideArticleContent } = require('./get-guide-article-content');
+
+const data = fs.readFileSync('D:/Coding/search-files/data/forum-topics-and-challenge-files-matrix.json', 'utf8');
+const matchedForumTopics = JSON.parse(data).matches;
+
+for (let { guideFilePath, title, forumTopicId } of matchedForumTopics) {
+  console.log(guideFilePath);
+  const guideArticleContent = getGuideArticleContent(guideFilePath);
+  if (guideArticleContent) {
+    const regex = /\s*(?<mainHeader># .+)\r?\n/;
+    const match = guideArticleContent.match(regex);
+    if (match) {
+      //console.log(match.groups.mainHeader);
+    } else {
+      console.log('could not find main header');
+    }
+  } else {
+    console.log(guideFilePath);
+    console.log('could not retrieve guide content');
+  }
+}
+
