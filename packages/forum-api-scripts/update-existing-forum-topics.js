88388ecea273b1373b7cae3c60f648afd6687844
Randell Dawson <rdawson@onepathtech.com>
2019-07-31 09:57:19 -0700

    fix: created consoleLog to clean up code

--- packages/forum-api-scripts/update-existing-forum-topics.js
+++ packages/forum-api-scripts/update-existing-forum-topics.js
@@ -3,19 +3,32 @@ const { delay } = require('./delay');
 const { getGuideArticleContent } = require('./get-guide-article-content');
 const { updateTopic } = require('./update-topic');
 
+const consoleLog = (forumTopicId, errors) => {
+  console.log('topic_id ' + forumTopicId + ' had issues while trying to update');
+  console.log(errors);
+  console.log();
+};
+
 const data = fs.readFileSync('./data/forum-topics-and-challenge-files-matrix.json', 'utf8');
 const matchedForumTopics = JSON.parse(data).matches;
 const scriptResults = [];
 
 (async () => {
-  for (let { title, forumTopicId} of matchedForumTopics) {
-    const result = await updateTopic(forumTopicId, title, guideArticleContent);
-    let toLog = { forumTopicId, challengeTitle, challengeFilePath, status: result.status };
-    if (result.status !== 'success') {
-      toLog = { ...toLog, errors: result.errors }
-      console.log('topic_id ' + forumTopicId + ' had issues while trying to update');
-      console.log(result.errors);
-      console.log();
+  for (let { challengeFilePath, guideFilePath, title, forumTopicId } of matchedForumTopics) {
+    const guideArticleContent = getGuideArticleContent(guideFilePath);
+    let toLog = { forumTopicId, title, challengeFilePath, guideFilePath };
+    if (guideArticleContent) {
+      const result = await updateTopic(forumTopicId, title, guideArticleContent);
+      toLog = { ...toLog, status: result.status };
+      if (result.status !== 'success') {
+        toLog = { ...toLog, errors: result.errors };
+        consoleLog(forumTopicId, result.errors);
+      }
+    // no guide content
+    } else {
+      const errMsg = 'no guide content found to update forum topic';
+      toLog = { ...toLog, errors: [ errMsg ] };
+      consoleLog(errMsg);
     }
     scriptResults.push(toLog);
     await delay(4000);
