01100806b7816546410a144cdf7e8a5a3a5ed641
Mrugesh Mohapatra <me@raisedadead.com>
2020-04-04 00:47:39 +0530

    fix(news): update script to v3 compatibility

--- news/get-user-emails.js
+++ news/get-user-emails.js
@@ -3,42 +3,51 @@ require("dotenv").config();
 const fs = require("fs");
 const path = require("path");
 
-const testConfig = () => {
-  if (!process.env.NEWS_API_URL || !process.env.NEWS_API_ADMIN_KEY) {
-    throw "Key are not configured.";
+const keys = {
+  user: {
+    role: process.env.NEWS_API_USER_ROLE || "Author",
+  },
+  api: {
+    url: process.env.NEWS_API_URL,
+    key: process.env.NEWS_API_ADMIN_KEY,
+    version: process.env.NEWS_API_VERSION || "v2"
   }
-};
-try {
-  testConfig();
-} catch (e) {
-  console.log(e);
 }
 
 const GhostAdminAPI = require("@tryghost/admin-api");
 
 const api = new GhostAdminAPI({
-  url: process.env.NEWS_API_URL || "http://localhost:2368",
-  version: "v2",
-  key: process.env.NEWS_API_ADMIN_KEY
+  ...keys.api
 });
 
-const createAuthorsDump = authors =>
+const createAuthorsDump = (authors) =>
   new Promise((resolve, reject) =>
     fs.writeFile(
-      path.resolve(__dirname, "authors.txt"),
+      path.resolve(__dirname, keys.user.role + ".json"),
       JSON.stringify(authors),
-      err => (err ? reject(err) : resolve())
+      (err) => (err ? reject(err) : resolve())
     )
   );
 
 const getUserEmails = () => {
   api.users
-    .browse({ limit: 250 }) // <---- Not a very good idea longterm
-    .then(authors => {
-      // const { pages, next } = authors.meta.pagination;
-      createAuthorsDump(authors.map(({ email, name }) => [email, name]));
+    .browse({ include: "roles", limit: "all" })
+    .then((users) => {
+      createAuthorsDump(
+        users
+          .map((user) => {
+            const { email, name: authorName, roles } = user;
+            return roles.map(({ name: roleName }) =>
+              roleName === keys.user.role
+                ? { email, name: authorName }
+                : null
+            );
+          })
+          .reduce((acc, cur) => acc.concat(cur))
+          .filter((user) => !!user)
+      );
     })
-    .catch(err => console.log(err));
+    .catch((err) => console.log(err));
 };
 
 getUserEmails();
