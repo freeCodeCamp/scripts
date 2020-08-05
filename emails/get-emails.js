e6dd0e6cafe5ca6302a2a3f60329530dc3e93798
Mrugesh Mohapatra <me@raisedadead.com>
2020-08-05 15:42:51 +0530

    fix: update packages and add progress indicators

--- emails/get-emails.js
+++ emails/get-emails.js
@@ -7,28 +7,31 @@ const assert = require('assert');
 const mongodb = require('mongodb');
 const validator = require('validator');
 const emailValidator = require('email-validator');
+const ora = require('ora');
 
 const MongoClient = mongodb.MongoClient;
 const filePath = process.argv[2];
 
 const validOutput = fs.createWriteStream(filePath, { encoding: 'utf8' });
-const invalidOutput = fs.createWriteStream('./invalidEmails.csv', { encoding: 'utf8' });
+const invalidOutput = fs.createWriteStream('./invalidEmails.csv', {
+  encoding: 'utf8'
+});
 
 validOutput.write('email,unsubscribeId\n');
 invalidOutput.write('email,unsubscribeId\n');
 
-const rs = new Stream.Readable({objectMode: true})
-rs._read = function() {}
+const rs = new Stream.Readable({ objectMode: true });
+rs._read = function () {};
 
-rs.on('data', ({email, unsubscribeId}) => {
-  if(validator.isEmail(email)) {
+rs.on('data', ({ email, unsubscribeId }) => {
+  if (validator.isEmail(email)) {
     validOutput.write(`${email},${unsubscribeId}\n`);
   } else if (emailValidator.validate(email)) {
     validOutput.write(`${email},${unsubscribeId}\n`);
   } else {
     invalidOutput.write(`${email},${unsubscribeId}\n`);
   }
-})
+});
 
 assert(
   filePath,
@@ -40,44 +43,67 @@ assert(
   `
 );
 
-const { MONGO_DB, MONGO_PASSWORD, MONGO_RS, MONGO_USER, MONGODB_URI } = process.env;
+const {
+  MONGO_DB,
+  MONGO_PASSWORD,
+  MONGO_RS,
+  MONGO_USER,
+  MONGODB_URI
+} = process.env;
 
 MongoClient.connect(
   MONGODB_URI,
   {
     useNewUrlParser: true,
+    useUnifiedTopology: true,
     replicaSet: MONGO_RS,
-    auth: { user: MONGO_USER, password: MONGO_PASSWORD}
+    auth: { user: MONGO_USER, password: MONGO_PASSWORD },
+    poolSize: 20
   },
-  function(err, client) {
+  function (err, client) {
     if (err) {
       throw err;
     }
-    const db = client.db(MONGO_DB)
+    const db = client.db(MONGO_DB);
 
-    const stream = db.collection('user').find({
-      $and: [
-        { email: { $exists: true } },
-        { email: { $ne: '' } },
-        { email: { $ne: null } },
-        { email: { $not: /(test|fake)/i } },
-        { $or: [
-            { sendQuincyEmail: true },
-            { sendQuincyEmail: { $exists: false } },
-            { sendQuincyEmail: null }
+    const stream = db
+      .collection('user')
+      .find(
+        {
+          $and: [
+            { email: { $exists: true } },
+            { email: { $ne: '' } },
+            { email: { $ne: null } },
+            { email: { $not: /(test|fake)/i } },
+            {
+              $or: [
+                { sendQuincyEmail: true },
+                { sendQuincyEmail: { $exists: false } },
+                { sendQuincyEmail: null }
+              ]
+            }
           ]
+        },
+        {
+          email: 1,
+          unsubscribeId: 1
         }
-      ]
-    }, {
-      email: 1,
-      unsubscribeId: 1
-    }).batchSize(100).stream();
+      )
+      .batchSize(100)
+      .stream();
 
-    stream.on('data', ({email, unsubscribeId}) => {
-      const data = {email, unsubscribeId};
-      rs.push(data)
-    })
+    const spinner = ora('Getting emails');
+    spinner.start();
 
-    stream.on('end', () => {rs.push(null);client.close()});
+    stream.on('data', ({ email, unsubscribeId }) => {
+      const data = { email, unsubscribeId };
+      spinner.text = `Getting details for: ${email}\n`;
+      rs.push(data);
+    });
 
-});
+    stream.on('end', () => {
+      rs.push(null);
+      client.close();
+    });
+  }
+);
