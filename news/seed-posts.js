d8d75a41725c2526d7e1ea87ef6298ff251c914e
Mrugesh Mohapatra <me@raisedadead.com>
2020-10-31 01:37:16 +0530

    logs

--- news/seed-posts.js
+++ news/seed-posts.js
@@ -18,6 +18,8 @@ const keys = {
   }
 };
 
+let postsAdded = 0;
+
 const apiGetter = new GhostAdminAPI({ ...keys.getter });
 const apiSetter = new GhostAdminAPI({ ...keys.setter });
 
@@ -25,8 +27,8 @@ const seedPosts = async () => {
   let currPage = 1;
   let lastPage = 1;
 
-  const spinner = ora('Begin seeding posts...');
-  spinner.start();
+  // const spinner = ora('Begin seeding posts...');
+  // spinner.start();
 
   while (currPage && currPage <= lastPage) {
     const data = await apiGetter.posts.browse({
@@ -37,11 +39,11 @@ const seedPosts = async () => {
     const posts = [...data];
 
     // Uncomment this when testing
-    // currPage = 2;
+    currPage = 2;
 
     // Comment these two when testing
-    currPage = data.meta.pagination.next;
-    lastPage = data.meta.pagination.pages;
+    // currPage = data.meta.pagination.next;
+    // lastPage = data.meta.pagination.pages;
 
     for (let i in posts) {
       const post = posts[i];
@@ -72,48 +74,65 @@ const seedPosts = async () => {
 
       // Handle tags differently to prevent duplicate tags
       const tags = post.tags.map(({ name, slug }) => ({ name, slug }));
+      const postToAdd = {
+        id,
+        uuid,
+        title,
+        slug,
+        mobiledoc,
+        // html,  // We do not need to get set this key during add calls, use force_rerender when doing edit calls
+        feature_image,
+        featured,
+        status,
+        locale,
+        visibility,
+        created_at,
+        updated_at,
+        published_at,
 
-      apiSetter.posts
-        .add({
-          id,
-          uuid,
-          title,
-          slug,
-          mobiledoc,
-          // html,  // We do not need to get set this key during add calls, use force_rerender when doing edit calls
-          feature_image,
-          featured,
-          status,
-          locale,
-          visibility,
-          created_at,
-          updated_at,
-          published_at,
+        // Some of these things while available on V2 can be overriden to these values
+        // because that's how we want them, for intance the canonical url should be null
+        custom_excerpt: null,
+        codeinjection_head: null,
+        codeinjection_foot: null,
+        custom_template: null,
+        canonical_url: null,
+        send_email_when_published: false,
 
-          // Some of these things while available on V2 can be overriden to these values
-          // because that's how we want them, for intance the canonical url should be null
-          custom_excerpt: null,
-          codeinjection_head: null,
-          codeinjection_foot: null,
-          custom_template: null,
-          canonical_url: null,
-          send_email_when_published: false,
+        tags,
+        primary_tag,
 
-          tags,
-          primary_tag,
+        authors,
+        primary_author
+      };
 
-          authors,
-          primary_author
-        })
-        .then((res) => {
-          spinner.text = `Seeded post: ${title}`;
+      await apiSetter.posts
+        .add(postToAdd)
+        .then(({ slug }) => {
+          // spinner.text = `Seeded post: ${title}`;
+          console.log(slug);
+          fs.appendFileSync(
+            'passed-posts.log',
+            `{
+  "time": "${new Date()}",
+  "post": ${JSON.stringify(postToAdd, null, 2)}
+},
+`,
+            { flag: 'a+', encoding: 'utf-8' }
+          );
+          postsAdded++;
         })
         .catch((err) => {
-          spinner.fail = `Failed seeding: ${title}`;
+          // spinner.fail = `Failed seeding: ${title}`;
           console.log('DISASTER', err);
           fs.appendFileSync(
-            'failed-posts.txt',
-            `Time:${new Date()} - Title: ${title} - Slug: ${slug} - Id: ${id}\n`,
+            'failed-posts.log',
+            `{
+  "time": "${new Date()}",
+  "post": ${JSON.stringify(postToAdd, null, 2)}
+  "error": ${JSON.stringify(err)}
+},
+`,
             { flag: 'a+', encoding: 'utf-8' }
           );
         });
@@ -121,7 +140,8 @@ const seedPosts = async () => {
       await wait(1);
     }
   }
-  spinner.succeed('Completed seeding posts.');
+  console.log(`Complete. ${postsAdded} posts added.`);
+  // spinner.succeed('Completed seeding posts.');
 };
 
 seedPosts();
