5edf34116d126b612a901b1ffc5d0c910a524514
Oliver Eyton-Williams <ojeytonwilliams@gmail.com>
2020-10-30 15:26:26 +0100

    fix: wip seeding

--- news/seed-posts.js
+++ news/seed-posts.js
@@ -1,41 +1,43 @@
-require('dotenv').config();
+require("dotenv").config();
 
-const GhostAdminAPI = require('@tryghost/admin-api');
-const ora = require('ora');
-const fs = require('fs');
-const { wait } = require('./utils');
+const GhostAdminAPI = require("@tryghost/admin-api");
+// const ora = require("ora");
+const fs = require("fs");
+const { wait } = require("./utils");
 
 const keys = {
   getter: {
     url: process.env.GETTER_NEWS_API_URL,
     key: process.env.GETTER_NEWS_API_ADMIN_KEY,
-    version: 'v2'
+    version: "v2",
   },
   setter: {
     url: process.env.SETTER_NEWS_API_URL,
     key: process.env.SETTER_NEWS_API_ADMIN_KEY,
-    version: 'v3'
-  }
+    version: "v3",
+  },
 };
 
 const apiGetter = new GhostAdminAPI({ ...keys.getter });
 const apiSetter = new GhostAdminAPI({ ...keys.setter });
 
+
 const seedPosts = async () => {
   let currPage = 1;
-  let lastPage = 5;
-  const spinner = ora('Begin seeding posts...');
-  spinner.start();
+  let lastPage = 1;
+  // const spinner = ora("Begin seeding posts...");
+  // spinner.start();
 
   while (currPage && currPage <= lastPage) {
     const data = await apiGetter.posts.browse({
       page: currPage,
-      formats: ['html', 'mobiledoc']
+      formats: ["html", "mobiledoc"],
     });
     const posts = [...data];
 
-    currPage = data.meta.pagination.next;
-    lastPage = data.meta.pagination.pages;
+    // currPage = data.meta.pagination.next;
+    currPage = 2;
+    // lastPage = data.meta.pagination.pages;
 
     for (let i in posts) {
       const post = posts[i];
@@ -54,14 +56,19 @@ const seedPosts = async () => {
         created_at,
         updated_at,
         published_at,
-
+        authors,
         primary_tag: primary_tag_v2,
-        primary_author: primary_author_v2
+        primary_author: primary_author_v2,
       } = post;
+      
+      const primary_tag = primary_tag_v2 ? { id: primary_tag_v2.id } : null;
+      const primary_author = primary_author_v2 ? { id: primary_author_v2.id } : null;
 
       // Handle tags differently to prevent duplicate tags
       const tags = post.tags.map((tag) => {
-        return ({ name, slug } = tag);
+     //   const { name, slug } = tag;
+        
+        return { name: tag.name, slug: tag.slug };
       });
 
       apiSetter.posts
@@ -91,27 +98,29 @@ const seedPosts = async () => {
           send_email_when_published: false,
 
           tags,
-          primary_tag: { id: primary_tag_v2.id },
+          primary_tag,
 
           authors,
-          primary_author: { id: primary_author_v2.id }
+          primary_author,
         })
         .then((res) => {
-          spinner.text = `Seeded post: ${title}`;
+          console.log('hello', res);
+          // spinner.text = `Seeded post: ${title}`;
         })
         .catch((err) => {
-          spinner.fail = `Failed seeding: ${title}`;
+          // spinner.fail = `Failed seeding: ${title}`;
+          console.log('disaster', err);
           fs.appendFileSync(
-            'failed-posts.txt',
+            "failed-posts.txt",
             `Title: ${title}, Id: ${id}\n`,
-            { flag: 'a+', encoding: 'utf-8' }
+            { flag: "a+", encoding: "utf-8" }
           );
         });
 
-      await wait(1);
+      // await wait(1);
     }
   }
-  spinner.succeed('Completed seeding posts.');
+  // spinner.succeed("Completed seeding posts.");
 };
 
 seedPosts();
