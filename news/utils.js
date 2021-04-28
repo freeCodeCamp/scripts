d147abc630a0013835aa02c634b22872e7dfa458
Nicholas Carrigan (he/him) <nhcarrigan@gmail.com>
2021-04-28 15:36:40 -0700

    feat: image replacement script (#25)
    Add a script to replace feature_images with new URL.

--- news/utils.js
+++ news/utils.js
@@ -1,7 +1,7 @@
 const WAIT_BETWEEN_CALLS = process.env.WAIT_BETWEEN_CALLS || 1;
 
-const wait = seconds => {
-  return new Promise(resolve => {
+const wait = (seconds) => {
+  return new Promise((resolve) => {
     setTimeout(() => {
       resolve(seconds);
     }, seconds * 1000);
@@ -19,32 +19,37 @@ const keys = {
   en: {
     url: process.env.NEWS_API_URL,
     key: process.env.NEWS_API_ADMIN_KEY,
-    version: process.env.NEWS_API_VERSION || 'v2',
+    version: process.env.NEWS_API_VERSION || "v2",
   },
   zh: {
     url: process.env.ZH_NEWS_API_URL,
     key: process.env.ZH_NEWS_API_ADMIN_KEY,
-    version: process.env.ZH_NEWS_API_VERSION || 'v2',
+    version: process.env.ZH_NEWS_API_VERSION || "v2",
   },
   es: {
     url: process.env.ES_NEWS_API_URL,
     key: process.env.ES_NEWS_API_ADMIN_KEY,
-    version: process.env.ES_NEWS_API_VERSION || 'v2',
+    version: process.env.ES_NEWS_API_VERSION || "v2",
   },
   getter: {
     url: process.env.GETTER_NEWS_API_URL,
     key: process.env.GETTER_NEWS_API_ADMIN_KEY,
-    version: 'v2'
+    version: "v2",
   },
   setter: {
     url: process.env.SETTER_NEWS_API_URL,
     key: process.env.SETTER_NEWS_API_ADMIN_KEY,
-    version: 'v3'
-  }
+    version: "v3",
+  },
+  images: {
+    url: process.env.IMAGES_API_URL,
+    key: process.env.IMAGES_API_ADMIN_KEY,
+    version: "v3",
+  },
 };
 
 module.exports = {
   wait,
   asyncForEach,
-  keys
-}
+  keys,
+};
