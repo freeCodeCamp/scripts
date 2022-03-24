d88787df89b33fe7408fd276c4358282e8ef20c4
Naomi Carrigan <nhcarrigan@gmail.com>
2022-03-24 08:36:49 -0700

    feat: improvements w/ kris + oliver

--- duplicates/src/merge.ts
+++ duplicates/src/merge.ts
@@ -1,9 +1,11 @@
-import { readFile } from "fs/promises";
+import { readFile, writeFile } from "fs/promises";
+import { createWriteStream } from "fs";
 import { join } from "path";
 
 import { MongoClient, ObjectId } from "mongodb";
 import Spinnies from "spinnies";
 import { userSchema } from "./schema";
+import fetch from "node-fetch";
 
 const spinnies = new Spinnies({
   spinner: {
@@ -46,13 +48,64 @@ const propertiesThatCanBeFalsy = [
   "twitter",
   "linkedin",
   "about",
+  "location",
   "isHonest",
   "isCheater",
   "acceptedPrivacyTerms",
   "isDonating",
+  "sendQuincyEmail",
+  "githubProfile",
+  "emailVerified",
+  "picture",
+  "isBanned",
 ];
 
+const defaultProfileUI = {
+  isLocked: true,
+  showAbout: false,
+  showCerts: false,
+  showDonation: false,
+  showHeatMap: false,
+  showLocation: false,
+  showName: false,
+  showPoints: false,
+  showPortfolio: false,
+  showTimeLine: false,
+};
+
 (async () => {
+  // validate that the properties being edited are still on the schema.
+  const userModelFromRepo = await fetch(
+    "https://raw.githubusercontent.com/freeCodeCamp/freeCodeCamp/main/api-server/src/common/models/user.json"
+  );
+  const userModel = await userModelFromRepo.json();
+  if (!certificationProperties.every((prop) => prop in userModel.properties)) {
+    throw new Error("Invalid certification keys.");
+  }
+  if (!propertiesThatCanBeFalsy.every((prop) => prop in userModel.properties)) {
+    throw new Error("Invalid falsy keys.");
+  }
+  if (
+    !Object.keys(defaultProfileUI).every(
+      (prop) => prop in userModel.properties.profileUI.default
+    )
+  ) {
+    throw new Error("Invalid profile UI");
+  }
+
+  const usernameLogPath = join(
+    process.cwd(),
+    "results",
+    "merged-usernames.csv"
+  );
+  const portfolioLogPath = join(
+    process.cwd(),
+    "results",
+    "merged-portfolios.csv"
+  );
+  const usernameLog = createWriteStream(usernameLogPath);
+  const portfolioLog = createWriteStream(portfolioLogPath);
+
   const dbClient = await MongoClient.connect(process.env.MONGO_URI as string, {
     replicaSet: "atlas-axsdig-shard-0",
     maxPoolSize: 20,
@@ -79,10 +132,65 @@ const propertiesThatCanBeFalsy = [
       )
     );
     const [first, ...rest] = documents.filter((el) => el) as userSchema[];
-    const firstChallengeIds = first.completedChallenges.map((el) => el.id);
-    const firstTimestamps = first.progressTimestamps;
+    // used for logging - email is not a username
+    const usernames = [first.email, first.username];
+
+    // arrays for merging
+    const yearsTopContributor = [...(first.yearsTopContributor || [])];
+    const profilePrivacySettings = first.profileUI || defaultProfileUI;
+
+    // portfolio logic
+    let hasPortfolio = !!first.portfolio?.length;
 
     for (const doc of rest) {
+      // store username in log
+      usernames.push(doc.username);
+
+      if (doc.yearsTopContributor?.length) {
+        yearsTopContributor.push(...doc.yearsTopContributor);
+      }
+
+      if (doc.profileUI) {
+        for (const key in doc.profileUI) {
+          // this key, when true, hides all the profile
+          if (key === "isLocked" && doc.profileUI[key]) {
+            profilePrivacySettings[key] = true;
+            continue;
+          }
+          // other keys hide specific parts when FALSE
+          if (!doc.profileUI[key]) {
+            profilePrivacySettings[key] = false;
+          }
+        }
+      }
+
+      // portfolios are merged, but need to be flagged for manual review (check if duplicate entries exist)
+      if (doc.portfolio?.length) {
+        if (!first.portfolio) {
+          first.portfolio = [];
+        }
+        first.portfolio.push(...doc.portfolio);
+        if (!hasPortfolio) {
+          hasPortfolio = true;
+        } else {
+          portfolioLog.write(`${doc.email}\n`);
+        }
+      }
+
+      const firstChallengeIds = first.completedChallenges.map((el) => el.id);
+      const firstTimestamps = first.progressTimestamps;
+
+      // Test for fcc702071be-2afd-4370-8483-620a52f7b602 or fcc05b15ec5 formats
+      const isDefaultUsername =
+        /fcc[\w\d]{8}/.test(first.username) ||
+        /fcc[\w\d]{8}-[\w\d]{4}-[\w\d]{4}-[\w\d]{4}-[\w\d]{12}/.test(
+          first.username
+        );
+
+      if (isDefaultUsername) {
+        first.username = doc.username;
+        first.usernameDisplay = doc.usernameDisplay;
+      }
       // filter, merge, sort completed challenges + progress timestamps
       const uniqueChallenges = doc.completedChallenges.filter(
         (el) => !firstChallengeIds.includes(el.id)
@@ -99,7 +207,7 @@ const propertiesThatCanBeFalsy = [
         ...uniqueTimestamps,
       ];
       const sortedChallenges = mergedChallenges.sort(
-        (a, b) => a.completedDate - b.completedDate
+        (a, b) => b.completedDate - a.completedDate
       );
       const sortedTimestamps = mergedTimestamps.sort((a, b) => a - b);
 
@@ -121,11 +229,14 @@ const propertiesThatCanBeFalsy = [
       }
 
       // delete duplicate
-      // await users.findOneAndDelete({ _id: new ObjectId(doc._id) });
+      await users.findOneAndDelete({ _id: new ObjectId(doc._id) });
     }
+    first.yearsTopContributor = [...new Set(yearsTopContributor)];
+    first.profileUI = profilePrivacySettings;
 
     // after running through all documents, update the first one
-    // await users.findOneAndUpdate({ _id: new ObjectId(first._id) }, first);
-    console.log(first);
+    await users.findOneAndReplace({ _id: new ObjectId(first._id) }, first);
+    usernames.push("FINAL: " + first.username);
+    usernameLog.write(`${usernames.join(",")}\n`);
   }
 })();
