1733a7c7dad03577bc5374f6a3457d7020a1868b
Randell Dawson <5313213+RandellDawson@users.noreply.github.com>
2022-07-23 00:36:32 -0600

    feat: Added table and pipe delimited output and more stats to Tricky Challenges report (#92)
    * chore: added nodemon and ts-node as dev packages
    
    * fix: added ability to group step counts
    
    * feat: added markdown output option
    
    * chore: sync package-lock.json
    
    Co-authored-by: Naomi Carrigan <nhcarrigan@gmail.com>

--- packages/tricky-challenges/src/index.ts
+++ packages/tricky-challenges/src/index.ts
@@ -2,11 +2,14 @@ import fetch from "node-fetch";
 
 import { Discourse, Topic } from "./interfaces/Discourse";
 
+import { Align, getMarkdownTable } from 'markdown-table-ts';
+
 (async () => {
   const topics: Topic[] = [];
   let page = 0;
   let limit = 100;
   let block = "";
+  let displayType = "";
 
   const specificLimit = process.argv.find((el) => el.startsWith("posts="));
   if (specificLimit) {
@@ -19,6 +22,13 @@ import { Discourse, Topic } from "./interfaces/Discourse";
   const specificBlock = process.argv.find((el) => el.startsWith("block="));
   if (specificBlock) {
     block = specificBlock.split("=")[1];
+  }
+
+  const projectsOnly = process.argv.find((el) => el.startsWith("project"));
+
+  const specificDisplay = process.argv.find((el) => el.startsWith("display"));
+  if (specificDisplay) {
+    displayType = specificDisplay.split("=")[1];
   }
 
   while (topics.length < limit) {
@@ -43,15 +53,56 @@ import { Discourse, Topic } from "./interfaces/Discourse";
 
   for (const title of defaultHelpTitles) {
     const [block, step] = title.split(/\s+-\s*/g);
-    const blockStepString = `${block}-${step}`;
+
+    let stepNum;
+    if (projectsOnly) {
+      stepNum = step.match(/step\s*\d+/i);
+    }
+    const blockStepString = projectsOnly
+      ? `${block}-${stepNum}`
+      : `${block}-${step}`;
     counts[blockStepString] = counts[blockStepString] + 1 || 1;
   }
+  
+  const dataArr = Object.entries(counts)
+    .filter((el) => el[0].startsWith(block))
+    .sort((a, b) => b[1] - a[1]);
 
-  block
-    ? console.log(
-        Object.entries(counts)
-          .filter((el) => el[0].startsWith(block))
-          .sort((a, b) => b[1] - a[1])
-      )
-    : console.log(Object.entries(counts).sort((a, b) => b[1] - a[1]));
+  const paretoCounts = dataArr.reduce<{
+    totalCount: number;
+    arr: [string, number, number][];
+  }>(
+    (dataObj, [title, freq]) => {
+      const totalCount = dataObj.totalCount + freq;
+      const { arr } = dataObj;
+      arr.push([title, freq, totalCount]);
+      return { totalCount, arr };
+    },
+    { totalCount: 0, arr: [] }
+  );
+  
+  const { totalCount, arr } = paretoCounts;
+  const outputArr = arr.map(([ title, freq, cumulativeCount ]) => {
+    const percent = (freq / totalCount * 100).toFixed(2);
+    const cumaltivePerc = (cumulativeCount / totalCount * 100).toFixed(2);
+    return [ title, String(freq), percent, cumaltivePerc ];
+  });
+ 
+ const strHeader = "Challenge/Step|Frequency|%|Cumulative %\n";
+ const tableHeader =["Challenge/Step", "Frequency", "%", "Cumulative %"];
+
+ const output = displayType === 'table'
+   ? getMarkdownTable({
+       table: {
+         head: tableHeader,
+         body: outputArr
+       },
+       alignment: [Align.Left, Align.Center, Align.Center, Align.Center]
+     })
+   : strHeader + outputArr.reduce((str, [topic, count, perc, cummaltivePerc]) => {
+       return str += `${topic}|${count}|${perc}|${cummaltivePerc}\n`;
+     }, '');   
+
+  console.log(output);
+
 })();
