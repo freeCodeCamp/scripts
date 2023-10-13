e905f9f041aa15b97e55134a712d01a7325792f3
sembauke <semboot699@gmail.com>
2023-10-13 14:49:51 +0200

    feat: format tags

--- news/strapi-migration.js
+++ news/strapi-migration.js
@@ -1,5 +1,36 @@
 require('dotenv').config();
+const fetch = require('node-fetch');
+const fs = require('fs');
 
-async function migrate(){
 
+async function formatTagData() {
+
+  const object = {
+    tags: []
+  }
+
+  const url = `${process.env.NEWS_API_URL}/ghost/api/v3/content/tags/?key=${process.env.NEWS_API_KEY}`;
+  const tags = await fetch(url).then(res => res.json());
+
+  const tagsModified = tags['tags'].map(tag => {
+    return {
+      id: tag.id,
+      name: tag.name,
+      slug: tag.slug,
+    }
+  })
+
+  object.tags = [...tagsModified]
+
+  // writing to file for now
+  fs.writeFile('./tags.json', JSON.stringify(object, null, 2), (err) => {
+    if (err) throw err;
+    console.log('The file has been saved!');
+  });
 }
+
+async function migrate() {
+  formatTagData();
+}
+
+migrate();
